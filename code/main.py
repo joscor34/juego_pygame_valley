c3e016c4689cbad7864641f7afc4b44dc4a73ba5
José Eduardo Esteva Córdova <zbrm34@gmail.com>
2024-01-26 18:54:41 -0600

    Se agregó el archivo principal de nuestro videojuego

---
+++ code/main.py
@@ -0,0 +1,33 @@
+import pygame, sys # Importamos pygame y sys
+
+# Comentario de prueba
+
+# Creamos una clase la cual contendrá nuestro juego
+class Game:
+
+  # Creamos un método mágico para inicializar nuestros atributos
+  def __init__(self):
+    pygame.init() # Iniciamos pygame
+    self.screen = pygame.display.set_mode((500,500)) # Le decimos a pygame que tendrá una ventana de 128x128
+    self.clock = pygame.time.Clock() # Creamos un reloj interno
+  
+  def ejecutar(self):
+    while True:
+      # Es ciclo recorre cada pulsación que haga el usuario
+      for evento in pygame.event.get():
+        # Si la pulsación coincide con el evento QUIT
+        if evento.type == pygame.QUIT:
+          # Le decimos a pygame que finalice
+          pygame.quit()
+          # Cerramos la ventaja que nos genere
+          sys.exit()
+      
+      dt = self.clock.tick() / 1000
+      pygame.display.update()
+
+# Se crea una instancia de Game
+game = Game()
+
+# Ejecutamos el método "ejecutar"
+game.ejecutar()
+
