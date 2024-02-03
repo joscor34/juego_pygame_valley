f999d1940bdc9e8df214877cf8e65e7b35f0f1fd
José Eduardo Esteva Córdova <zbrm34@gmail.com>
2024-02-02 18:42:38 -0600

    Agregamos el grupo de sprites a nuestro videojuego

--- code/level.py
+++ code/level.py
@@ -1,5 +1,20 @@
 import pygame
 
 class Level():
+  def __init__(self):
+
+    # Se crea la superficie de nuestra pantalla
+    self.display_surface = pygame.display.get_surface()
+
+    # Creamos un grupo con todos los sprites del nivel
+    self.all_sprites = pygame.sprite.Group()
+  
+
   def run(self, dt):
-    print('Nivel corriendo')
+
+    self.display_surface.fill('red')
+
+    # Se dibujan nuestros sprites en toda la superficie de la pantalla
+    self.all_sprites.draw(self.display_surface)
+    # Actualizamos esos sprites
+    self.all_sprites.update()
