1d31d7763d02719284ea35d1df80810d8a369ed4
José Eduardo Esteva Córdova <zbrm34@gmail.com>
2024-02-16 17:11:48 -0600

    Se agrego el archivo para el personaje

--- code/player.py
+++ code/player.py
@@ -1,14 +1,18 @@
 import pygame
 from settings import *
+from support import *
+from os import walk
 
 class Player(pygame.sprite.Sprite):
   def __init__(self, pos, group):
     super().__init__(group)
 
+    self.import_assets()
+    self.status = 'down_idle'
+    self.frame_index = 0
     # Se configuran las propiedades de nuestro personaje
-    self.image = pygame.Surface((32,64))
-    self.image.fill('black')
-
+    self.image = self.animations[self.status][self.frame_index]
+    #self.image.fill('black')
     self.rect = self.image.get_rect(center = pos)
 
     # Configuración para el movimiento
@@ -16,9 +20,23 @@ class Player(pygame.sprite.Sprite):
     
     self.pos = pygame.math.Vector2(self.rect.center)
 
-    self.speed = 100
+    self.speed = 350
 
-    # (x,y)
+    
+  def import_assets(self):
+    self.animations = {'up':[], 'down': [], 'left':[], 'right':[],
+                       'up_idle': [], 'down_idle': [], 'left_idle':[], 'right_idle':[],
+                       'up_axe': [], 'down_axe': [], 'left_axe':[], 'right_axe':[],
+                       'up_hoe': [], 'down_hoe': [], 'left_hoe':[], 'right_hoe':[],
+                       'up_water': [], 'down_water': [], 'left_water':[], 'right_water':[],
+                      }
+
+    for animation in self.animations.keys():
+      ruta_completa = './graphics/character/' + animation
+
+      self.animations[animation] = import_folder(ruta_completa)
+    
+    print(self.animations['up'])
 
 
   def input(self):
@@ -44,11 +62,21 @@ class Player(pygame.sprite.Sprite):
       self.direction.x = 0
     # --------------------------------------
 
-    print(self.direction)
+    
   
   def move(self, dt):
-    self.pos += self.direction * self.speed * dt
-    self.rect.center = self.pos
+
+    # Se hace la normalización de nuestros vectores de movimiento
+    if self.direction.magnitude() > 0:
+      self.direction = self.direction.normalize()
+
+    # Movimiento horizontal
+    self.pos.x += self.direction.x * self.speed * dt
+    self.rect.centerx = self.pos.x
+
+    # Movimiento vertical
+    self.pos.y += self.direction.y * self.speed * dt
+    self.rect.centery = self.pos.y
 
   def update(self, dt):
     self.input()
