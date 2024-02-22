6f980cb9f89c1f0f95fe562f4102a3ac77912eb0
José Eduardo Esteva Córdova <zbrm34@gmail.com>
2024-02-22 10:33:55 -0600

    Se agrego una pureba para demostrar que timer no funciona como temporizador

--- code/player.py
+++ code/player.py
@@ -2,12 +2,14 @@ import pygame
 from settings import *
 from support import *
 from os import walk
+import time
 
 class Player(pygame.sprite.Sprite):
   def __init__(self, pos, group):
     super().__init__(group)
 
     self.import_assets()
+
     self.status = 'down_idle'
     self.frame_index = 0
     # Se configuran las propiedades de nuestro personaje
@@ -36,7 +38,13 @@ class Player(pygame.sprite.Sprite):
 
       self.animations[animation] = import_folder(ruta_completa)
     
-    print(self.animations['up'])
+
+  def animate(self, dt):
+    self.frame_index += 4 * dt
+    if self.frame_index >= len(self.animations[self.status]):
+      self.frame_index = 0
+
+    self.image = self.animations[self.status][int(self.frame_index)]
 
 
   def input(self):
@@ -45,24 +53,38 @@ class Player(pygame.sprite.Sprite):
     # --------------------------------------
     if keys[pygame.K_w]:
       self.direction.y = -1
+      self.status = 'up'
 
     elif keys[pygame.K_s]:
       self.direction.y = 1
+      self.status = 'down'
 
     else:
       self.direction.y = 0
     # --------------------------------------
     if keys[pygame.K_a]:
       self.direction.x = -1
+      self.status = 'left'
 
     elif keys[pygame.K_d]:
       self.direction.x = 1
+      self.status = 'right'
 
     else:
       self.direction.x = 0
     # --------------------------------------
+      
 
-    
+    if keys[pygame.K_SPACE]:
+      time.sleep(4)
+
+  def get_status(self):
+
+    # Si mi personaje no se está desplazando
+    if  self.direction.magnitude() == 0:
+      #print(self.status + '_idle')
+      self.status = self.status.split('_')[0] + '_idle'
+
   
   def move(self, dt):
 
@@ -80,4 +102,6 @@ class Player(pygame.sprite.Sprite):
 
   def update(self, dt):
     self.input()
+    self.get_status()
     self.move(dt)
+    self.animate(dt)
